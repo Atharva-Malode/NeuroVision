b51d19fb07cdcd02df308c39a05e3070985552bd
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-08-28 21:27:40 +0530

    Route: Added path to AboutUs Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import  { Navbar } from "./components/header";
 import Footer from "./components/footer";
 import Doctors from "./Pages/Doctors/index";
-
+import AboutUs from "./Pages/AboutUs/index";
 function App() {
   return (
     <BrowserRouter>
       
       <Navbar />
       <Routes>
+        <Route eaxct path="/about" element={<AboutUs/>}/>
         {/* <Route exact path="/" element={<Landing />} /> */}
-        <Route exact path = "/Doctors" element = {<Doctors/>} />
+        {/* <Route exact path = "/Doctors" element = {<Doctors/>} /> */}
       </Routes>
       <Footer />
       
