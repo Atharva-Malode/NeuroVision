4c3946141b709654c361aa169161399095ad577d
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-10-26 15:29:27 +0530

    update : changed font color and added gap of four

--- Frontend/src/Pages/Home/predict.jsx
+++ Frontend/src/Pages/Home/predict.jsx
@@ -12,7 +12,7 @@ const Predict = () => {
       <div className="text-2xl font-bold text-black content-center mb-8">
         <h1>Opthalmic Disease Screening</h1>
       </div>
-      <div className="flex flex-row bg-gray-200 p-8 rounded-lg shadow-lg text-center md:w-1/2 lg:w-2/3">
+      <div className="flex gap-4 flex-row bg-gray-200 p-8 rounded-lg shadow-lg text-center md:w-1/2 lg:w-2/3">
         <div className="border border-gray-400 p-4 rounded-lg mb-4">
           {imageUrl ? (
             <img
@@ -24,12 +24,12 @@ const Predict = () => {
             <p>No image selected.</p>
           )}
         </div>
-        <div className="flex-grow mt-4">
-          <h1 className="text-black content-center mb-4 ml-10">
+        <div className="flex-grow mt-4 column-1">
+          <h1 className="text-black content-center mb-4">
             As per the fundus image you uploaded following are the results:
           </h1>
           {prediction && predictedProbability && (
-            <div>
+            <div className="font-semibold p-2 text-center text-blue-900">
               <p>Prediction: {prediction}</p>
               <p>Predicted Probability: {predictedProbability}</p>
             </div>
